b1adf7726faf5372580d4374735e92b66d4569a1
1f10220158 <107387136+1f10220158@users.noreply.github.com>
2023-12-01 19:15:23 +0900

    モデル作成

--- app/models.py
+++ app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Analysis(models.Model):
+    id = models.AutoField(primary_key=True)
+    user_name = models.CharField(max_length=255)
+    datetime = models.DateTimeField(default= timezone.now)
+    vocal_path = models.FileField(upload_to='vocal/')
+    no_vocal_path = models.FileField(upload_to='no_vocal/')
+    bass_path = models.FileField(upload_to='bass/')
+    drum_path = models.FileField(upload_to='drum/')
+    other_path = models.FileField(upload_to='other/')
+    youtube_url = models.CharField(max_length=255)
+    music_path = models.FileField(upload_to='music/')
+    music_key = models.CharField(max_length=2)
+    music_title = models.CharField(max_length=255)
+    vocal_range = models.CharField(max_length=7)
+
