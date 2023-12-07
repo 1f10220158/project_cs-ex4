4c24085d7392b38986ec15a2109e9b0e3117d9a5
s1f10220252 <107392875+s1f10220252@users.noreply.github.com>
2023-12-07 17:53:46 +0900

    2023/12/07/17:53

---
+++ app/static/js/home.js
@@ -0,0 +1,8 @@
+function showPage(page) {
+    alert(`移動先: ${page}`);
+  }
+  
+  function showDetails(title) {
+    document.getElementById('detailsContent').innerText = `${title} の詳細がここに表示されます。`;
+  }
+  
