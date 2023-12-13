c9d09790c92c637cb5277edf6df2229a8e856344
1f10220158 <107387136+1f10220158@users.noreply.github.com>
2023-12-13 09:28:42 +0900

    update anaysis.html, views.py

--- app/views.py
+++ app/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+import soundfile
 
-@login_required
+#@login_required
 def private(request):
     return render(request, "private.html")
 
-@login_required
+#@login_required
 def analysis(request):
+    if request.method == "GET":
+        return render(request, "analysis.html")
+    
+    elif request.method == "POST":
+        file = request.FILES.get("soundfile")
+        url = request.POST.get("url")
+        vocal = request.POST.get("vocal")
+        separate = request.POST.get("separate")
+        soundfile.write(file='test.wav', data=file ,samplerate=14400)
+
     return render(request, "analysis.html")
 
 def signUp(request):
