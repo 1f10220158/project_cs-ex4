17b91412e301aa71115b48d922d1e3042abd7473
s1f102200277 <107395757+s1f102200277@users.noreply.github.com>
2023-12-14 18:18:09 +0900

    commit2

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from . import views
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('', views.home, name="home"),
@@ -8,5 +9,4 @@ urlpatterns = [
     path('analysis/', views.analysis, name="analysis"),
     path('signup/', views.signup, name="signUp"),
      path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
 ]
--- app/views.py
+++ app/views.py
@@ -47,7 +47,7 @@ def login_view(request):
 
 def logout_view(request):
     logout(request)
-    return render(request, 'registration/loin.html')
+    return redirect('home') 
 
 def signup(request):
     if request.method == 'POST':
